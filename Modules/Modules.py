6c5ca0e342a60c0359b2be4593ed04aa3ed4da60
pixsele <kirill2005vorob200@yandex.ru>
2023-09-09 17:49:08 +0400

    Исправления по дз модили

--- Modules/Modules.py
+++ Modules/Modules.py
@@ -1,21 +1,33 @@
-import my_calc
+from my_calc import *
 
-a = int(input('Введите первое число выражения : '))
-b = int(input('Введите второе число выражения : '))
-
-print('Выберите математическую операцию из списка')
-print('1 - Сложение\n2 - Вычетание\n3 - Умножение\n4 - Деление')
-operation = input('Введите её номер : ')
-
-if operation == '1':
-    answer = my_calc.add(a,b)
-elif operation == '2':
-    answer = my_calc.subtract(a,b)
-elif operation == '3':
-    answer = my_calc.multiply(a,b)
-elif operation == '4':
-    answer = my_calc.divide(a,b)
-else:
-    raise ValueError('Некорректная операция')
-
-print(f'Результат выражения = {answer}')
+cont = 1
+correct = True
+while cont == 1:
+    while correct == True:
+        try:
+            a = int(input('Введите первое число выражения : '))
+            b = int(input('Введите второе число выражения : '))
+            correct = False
+        except ValueError:
+            print('Введите число!')
+            correct = True
+    print('Выберите математическую операцию из списка')
+    print('1 - Сложение\n2 - Вычетание\n3 - Умножение\n4 - Деление')
+    operation = input('Введите её номер : ')
+    
+    if operation == '1':
+        answer = add(a,b)
+    elif operation == '2':
+        answer = subtract(a,b)
+    elif operation == '3':
+        answer = multiply(a,b)
+    elif operation == '4':
+        answer = divide(a,b)
+    else:
+        raise ValueError('Некорректная операция')
+    
+    print(f'Результат выражения = {answer}')
+    print('Хотети продолжить?\nНажмите:\n1 - для продолжения\n2 - для выхода')
+    cont = int(input())
+    correct = True
+    
